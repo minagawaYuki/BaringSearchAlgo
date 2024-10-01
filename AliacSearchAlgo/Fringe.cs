63b650d5915729c9016dbcb39a96e99a03e8687b
minagawaYuki <markadrian.baring@cit.edu>
2024-10-01 17:43:59 +0800

    Add project files.

---
+++ AliacSearchAlgo/Fringe.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AliacSearchAlgo;
+
+namespace AISearchSample
+{
+    class Fringe:Fringes
+    {
+        Stack<Node> stack;
+        Queue<Node> queue;
+        public Fringe() 
+        {
+            stack = new Stack<Node>();
+            queue = new Queue<Node>();
+        }
+        
+        public void add(Node n,Node origin)
+        {
+            n.Origin = origin;
+            stack.Push(n);
+            queue.Enqueue(n);
+        }
+
+        public Node remove()
+        {
+            if (stack.Count != 0)
+                return stack.Pop();
+            if (queue.Count != 0)
+                return queue.Dequeue();
+            return null;
+        }
+    }
+}
